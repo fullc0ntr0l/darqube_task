d11b6de9a9d71713b2f4ee6540594a383ae731c0
schneider <flywithjohnny@gmail.com>
2020-10-19 14:06:10 +0300

    Updates

---
+++ precache-manifest.c511298f56d56dde3cdbdeafa3a0a8ad.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "344658b10a5fbe96afe931a841d4dda1",
+    "url": "/darqube_task/index.html"
+  },
+  {
+    "revision": "525e3669a8127db4834a",
+    "url": "/darqube_task/static/css/main.c1774905.chunk.css"
+  },
+  {
+    "revision": "28122bb58de3d26e3037",
+    "url": "/darqube_task/static/js/2.a5f21a92.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/darqube_task/static/js/2.a5f21a92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "525e3669a8127db4834a",
+    "url": "/darqube_task/static/js/main.8cce2d3d.chunk.js"
+  },
+  {
+    "revision": "b8d4adcd23f2d5cd96ea",
+    "url": "/darqube_task/static/js/runtime-main.d8690308.js"
+  },
+  {
+    "revision": "7d423f6947cc91471efd1312febe6f89",
+    "url": "/darqube_task/static/media/bookmarkAdd.7d423f69.svg"
+  },
+  {
+    "revision": "94370750d2700fbe924d3ab13d9e1203",
+    "url": "/darqube_task/static/media/bookmarkAdded.94370750.svg"
+  },
+  {
+    "revision": "008848ddebe4dea2a7d192f8dd52c5c3",
+    "url": "/darqube_task/static/media/rightArrow.008848dd.svg"
+  },
+  {
+    "revision": "72297632d7a0abd32a179063ee2a31fe",
+    "url": "/darqube_task/static/media/search.72297632.svg"
+  },
+  {
+    "revision": "25d94ebc7be3b211acc34b3f72488373",
+    "url": "/darqube_task/static/media/upperRightArrow.25d94ebc.svg"
+  }
+]);
